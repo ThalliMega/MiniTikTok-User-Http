3b0305cdc87ea22821cc0e49e799dcabd2e20f7a
Thallium Megalovania <thallimega@foxmail.com>
2023-02-20 19:07:05 +0800

    refactor: api change
    Signed-off-by: Thallium Megalovania <thallimega@foxmail.com>

--- User/src/user_service.rs
+++ User/src/user_service.rs
@@ -3,14 +3,11 @@ use axum::{
     extract::{Query, State},
     Json,
 };
-use log::{error, warn};
+use log::error;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    proto::{
-        auth_response::AuthStatusCode, token_response::TokenStatusCode,
-        user_info_response::UserInfoStatusCode, AuthRequest, TokenRequest, UserInfoRequest,
-    },
+    proto::{auth_response::AuthStatusCode, token_response::TokenStatusCode, *},
     user_regist::{bolt_regist, postgres_regist},
     SharedState,
 };
@@ -128,12 +125,24 @@ struct UserInfo {
     follow_count: i64,
     follower_count: i64,
     is_follow: bool,
+    avatar: String,
+    background_image: String,
+    signature: String,
+    total_favorited: i64,
+    work_count: i64,
+    favorite_count: i64,
 }
 
 pub(super) async fn info(
     State(mut conns): State<SharedState>,
     Query(q): Query<InfoReq>,
 ) -> Json<InfoRes> {
+    let bad_gateway = Json(InfoRes {
+        status_code: 502,
+        status_msg: "Bad Gateway",
+        user: None,
+    });
+
     let user_id = match auth(q.token, conns.auth_client).await {
         Some(id) => id,
         None => {
@@ -145,52 +154,178 @@ pub(super) async fn info(
         }
     };
 
-    match conns
+    // TODO: refactor needed: parallel async
+    let info = match conns
         .user_client
-        .get_info(UserInfoRequest {
-            target_id: q.user_id,
-            self_id: user_id,
+        .get_infos(UserIds {
+            user_ids: [user_id].into(),
         })
         .await
     {
         Ok(r) => {
             let res = r.into_inner();
-            match res.status_code() {
-                UserInfoStatusCode::Success => Json(InfoRes {
-                    status_code: 0,
-                    status_msg: "Sucess",
-                    user: Some(UserInfo {
-                        id: res.user_id,
-                        name: res.username,
-                        follow_count: res.follow_count,
-                        follower_count: res.follower_count,
-                        is_follow: res.is_follow,
-                    }),
-                }),
-                UserInfoStatusCode::Unspecified => {
-                    warn!("user info http service received an unspecified status");
-                    Json(InfoRes {
-                        status_code: 500,
-                        status_msg: "Can't recognize the status code sent by gateway.",
-                        user: None,
-                    })
-                }
-                UserInfoStatusCode::TargetNotFound => Json(InfoRes {
+            if let Some(res) = res.infos.get(0) {
+                res.clone()
+            } else {
+                return Json(InfoRes {
                     status_code: 404,
                     status_msg: "Not Found",
                     user: None,
-                }),
+                });
             }
         }
         Err(e) => {
             error!("{e}");
-            Json(InfoRes {
-                status_code: 502,
-                status_msg: "Bad Gateway",
-                user: None,
-            })
+            return bad_gateway;
         }
-    }
+    };
+
+    let follow_count = match conns
+        .user_client
+        .get_follow_counts(UserIds {
+            user_ids: [user_id].into(),
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            if let Some(res) = res.responses.get(0) {
+                res.value
+            } else {
+                error!("no follow count");
+                0
+            }
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    let follower_count = match conns
+        .user_client
+        .get_follower_counts(UserIds {
+            user_ids: [user_id].into(),
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            if let Some(res) = res.responses.get(0) {
+                res.value
+            } else {
+                error!("no follower count");
+                0
+            }
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    let favorite_count = match conns
+        .user_client
+        .get_favorite_counts(UserIds {
+            user_ids: [user_id].into(),
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            if let Some(res) = res.responses.get(0) {
+                res.value
+            } else {
+                error!("no favorite count");
+                0
+            }
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    let total_favorited = match conns
+        .user_client
+        .get_total_favoriteds(UserIds {
+            user_ids: [user_id].into(),
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            if let Some(res) = res.responses.get(0) {
+                res.value
+            } else {
+                error!("no total favorited");
+                0
+            }
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    let work_count = match conns
+        .user_client
+        .get_work_counts(UserIds {
+            user_ids: [user_id].into(),
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            if let Some(res) = res.responses.get(0) {
+                res.value
+            } else {
+                error!("no work count");
+                0
+            }
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    let is_follow = match conns
+        .user_client
+        .check_follows(FollowCheckRequests {
+            self_id: user_id,
+            target_ids: vec![q.user_id],
+        })
+        .await
+    {
+        Ok(r) => {
+            let res = r.into_inner();
+            // Believe that db not broken; id == target_id omitted
+            res.target_ids.get(0).is_some()
+        }
+        Err(e) => {
+            error!("{e}");
+            return bad_gateway;
+        }
+    };
+
+    Json(InfoRes {
+        status_code: 0,
+        status_msg: "Success",
+        user: Some(UserInfo {
+            id: info.id,
+            name: info.username,
+            follow_count,
+            follower_count,
+            is_follow,
+            avatar: info.avatar,
+            background_image: info.background_img,
+            signature: info.signature,
+            total_favorited,
+            work_count,
+            favorite_count,
+        }),
+    })
 }
 
 async fn auth(
